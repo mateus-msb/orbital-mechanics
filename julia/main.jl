4d39a908d9325d2041084e3e994d3cbac29452c0
mateus-msb <mateus.silvaborges99@gmail.com>
2024-02-11 07:51:56 -0300

    Added basic structure in comments

---
+++ julia/main.jl
@@ -0,0 +1,15 @@
+# Input variables into Orbit struct from classical 
+# orbital elements or position and velocity 
+
+
+# Define simulation time and conditions
+
+
+# Tune control law used
+
+# Call function to simulate orbit
+
+
+
+
+# Plot graphics and return coordinates in ECI frame
