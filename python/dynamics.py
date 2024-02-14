99e283cc87de8e1b1d98104c5ae13615cec2130e
mateus-msb <mateus.silvaborges99@gmail.com>
2024-02-14 04:15:52 -0300

    Explain variable names and separated perturbation dynamics

--- python/dynamics.py
+++ python/dynamics.py
@@ -1,47 +1,92 @@
 import numpy as np
+from satellite import Satellite
+from orbit import Orbit
 
 
-def equation_of_motion(t,y,Orbit,Satellite):
+def equation_of_motion(t,y,Orbit: Orbit,Satellite: Satellite):
         """
-        Variables:
-        t:  time
-        y:  state (X,Y,Z,vx,vy,vz)
-        dy: state (vx,vy,vz,ax,ay,az)
+        Input variables:
+        t:  time (to be used by the ODE solver)
+        y:  state [X,Y,Z,vx,vy,vz]
+        dy: state [vx,vy,vz,ax,ay,az]
+
+        This function uses the Encke's method for
+        orbital perturbations
         """
+        
+        """
+        Variables (ECI):
+        r:  position vector [X,Y,Z]
+        v:  velocity vector [vx,vy,vz]
+        r_norm: Euclidean norm of the position vector
+        v_norm: Euclidean norm of the velocity vector
+        
+        R:  Radius of the central body
+        mu: Gravitational parameter of the central body
+        we: Central body radial velocity
+
+        a0:     acceleration caused by the main gravitational forces
+        a_drag: acceleration caused by the atmospheric drag
+        a_J2:   acceleration caused by the central body oblateness (J2)
+        """
+
         X,Y,Z,vx,vy,vz = y
 
         R  = Orbit.R
         mu = Orbit.mu
         we = np.array([0,0,Orbit.we])
         drag = Orbit.Drag
-
-        mass = Satellite.mass
-        CD   = Satellite.CD
-        area = Satellite.area
+        J2   = Orbit.J2
 
         r = np.array([X,Y,Z])
         v = np.array([vx,vy,vz])
         r_norm = np.sqrt(X**2 + Y**2 + Z**2)
-        
-        Vrel        = v - np.cross(we,r)
-        Vrel_norm   = np.sqrt(Vrel[0]**2 + Vrel[1]**2 + Vrel[2]**2)
-        Vrel_versor = Vrel / Vrel_norm
-        
-        altitude = r_norm - R
-        rho      = atmosphere(altitude)
 
-        a_drag = - (CD*area/mass) * (rho * (1000*Vrel_norm)**2 / 2) * Vrel_versor
-        
         a0 = -(mu/r_norm**3)*r
-        
-        a = a0 + a_drag / 1000 * drag
-        dy = np.concatenate((v,a))
+        a = a0
 
+        if drag:
+            a = a + a_drag(r,v,we,R,Satellite)
+
+        if J2:
+            a = a + a_J2(r,r_norm,R,mu,J2)
+        
+        dy = np.concatenate((v,a))
         return dy
 
 
+def a_drag(r: np.ndarray,v: np.ndarray, we: np.ndarray, R: float, Satellite: Satellite):
+    mass = Satellite.mass
+    cd   = Satellite.cd
+    area = Satellite.area
+
+    r_norm = np.sqrt(r[0]**2 + r[1]**2 + r[2]**2)
+
+    Vrel        = v - np.cross(we,r)
+    Vrel_norm   = np.sqrt(Vrel[0]**2 + Vrel[1]**2 + Vrel[2]**2)
+    Vrel_versor = Vrel / Vrel_norm
+    
+    altitude = r_norm - R
+    rho      = atmosphere(altitude)
+
+    a_drag = - 0.5 * (cd*area/mass) * rho * (1000*Vrel_norm)**2 * Vrel_versor / 1000
+    return a_drag
+
+
+def a_J2(r: np.ndarray,r_norm: float, R: float, mu: float, J2: float):
+    X,Y,Z = r
+    
+    fac = 3/2*J2*(mu/r_norm**2)*(R/r_norm)**2
+    vec = np.array([(1 - 5*(Z/r_norm)**2)*(X/r_norm), (1 - 5*(Z/r_norm)**2)*(Y/r_norm), (3 - 5*(Z/r_norm)**2)*(Z/r_norm)])
+    a_J2 = - fac*vec
+    return a_J2
+
 
 def atmosphere(z):
+    """
+    U.S. Standard Atmosphere 1976
+    Curtis - D.41
+    """
     # Geometric altitudes (km)
     h = np.array([0, 25, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140,
                   150, 180, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800,
