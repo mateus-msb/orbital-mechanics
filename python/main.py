efd35371052f74d911a7b95aab0b9e303bf84cf2
mateus-msb <mateus.silvaborges99@gmail.com>
2024-02-23 04:27:56 -0300

    Tested Curtis Example 7.2

--- python/main.py
+++ python/main.py
@@ -28,22 +28,21 @@ from math import pi
 
 # %%
 # Curtis - Example 4.3
-a = 6578
-e = 0.1712
-i = 0
-w = 20.07
-RA = 255.3
-TA = 28.45
+ra = [-266.77,+3865.8,+5426.2]
+va = [-6.4836,-3.6198,+2.4156]
+rb = [-5890.7,-2979.8,+1792.2]
+vb = [0.93583,-5.2403,-5.5009]
 
-orbit0 = orbit.OrbitElements(a,e,i,w,RA,TA)
-orbit1 = orbit.OrbitElements(a,e,i+50,w,RA,TA)
+orbit0 = orbit.OrbitStateVector(ra,va)
+orbit1 = orbit.OrbitStateVector(rb,vb)
 
 R, mu = orbit0.get_central_body()       # Get central body according to orbit
-orbit0.activate_perturbation(["J2"])  # Select perturbations
-orbit1.activate_perturbation(["J2"])  # Select perturbations
 
-sat0 = Satellite(orbit0,name="Leader")
-sat1 = Satellite(orbit1,name="follower")
+orbit0.activate_perturbation([])  # Select perturbations
+orbit1.activate_perturbation([])  # Select perturbations
+
+sat0 = Satellite(orbit0,mass=100,cd=2.2,area=pi*0.5**2,name="Leader")
+sat1 = Satellite(orbit1,mass=100,cd=2.2,area=pi*0.5**2)
 
 satellites = [sat0, sat1]
 
@@ -53,8 +52,8 @@ satellites = [sat0, sat1]
 # %%
 # Define simulation time in seconds
 t0        = 0
-time_end  = 3600*24
-time_step = 10
+time_end  = 3600*24*4
+time_step = 60
 times     = np.arange(0,time_end,step=time_step)
 
 # %% [markdown]
@@ -91,9 +90,71 @@ for time in times:
 # ## Plots
 
 # %%
-plot_error(satellites,times)
+plot_position(satellites,times)
+
+
+# %%
+plot_altitude(satellites[0],times,R)
+
 
 # %%
 plot3d(satellites,times)
+
+
+# %%
+X0,Y0,Z0,Vx0,Vy0,Vz0 = sat0.get_state()
+X1,Y1,Z1,Vx1,Vy1,Vz1 = sat1.get_state()
+x_rel = [];y_rel = []; z_rel = []
+vx_rel = [];vy_rel = []; vz_rel = []
+
+for idx in range(len(times)):
+    rA = [X0[idx],Y0[idx],Z0[idx]]
+    vA = [Vx0[idx],Vy0[idx],Vz0[idx]]
+    rB = [X1[idx],Y1[idx],Z1[idx]]
+    vB = [Vx1[idx],Vy1[idx],Vz1[idx]]
+    
+    rA_norm = np.sqrt(X0[idx]**2+Y0[idx]**2+Z0[idx]**2)
+
+    hA = np.cross(rA,vA)
+    i = rA / rA_norm
+    k = hA / np.sqrt(hA[0]**2+hA[1]**2+hA[2]**2)
+    j = np.cross(k,i)
+
+    Q = [i,j,k]
+
+    Omega = hA/rA_norm**2
+    dOmega = (-2 / rA_norm**2) * np.dot(np.dot(vA,rA),Omega)
+
+    r_rel_ECI = np.subtract(rB, rA)
+    v_rel_ECI = np.subtract(np.subtract(vB, vA),np.cross(Omega,r_rel_ECI))
+
+    r_rel_LVLH = Q @ r_rel_ECI
+    v_rel_LVLH = Q @ v_rel_ECI
+
+    x_rel.append(r_rel_LVLH[0])
+    y_rel.append(r_rel_LVLH[1])
+    z_rel.append(r_rel_LVLH[2])
+    vx_rel.append(v_rel_LVLH[0])
+    vy_rel.append(v_rel_LVLH[1])
+    vz_rel.append(v_rel_LVLH[2])
+
+
+# %%
+tri = plt.figure(32)
+ax = tri.add_subplot(111,projection='3d')
+ax.plot(x_rel,y_rel,z_rel)
+ax.set_xlabel("X (km)")
+ax.set_xlim([-10000,2000])
+ax.set_ylim([-7000,7000])
+ax.set_zlim([-4000,4000])
+
+# Draw X-axis arrow
+ax.quiver(0, 0, 0, 1, 0, 0, color='r', label='X', length=5000, arrow_length_ratio=.1)
+# Draw Y-axis arrow
+ax.quiver(0, 0, 0, 0, 1, 0, color='r', label='Y', length=5000, arrow_length_ratio=.1)
+# Draw Z-axis arrow
+ax.quiver(0, 0, 0, 0, 0, 1, color='r', label='Z', length=5000, arrow_length_ratio=.1)
+
+plt.show()
 
 
